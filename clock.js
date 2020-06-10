cc64763a49f0ea676e98d98e5960a8ee83c633c9
Nick Hvattum <nhvattum@gmail.com>
2020-06-09 19:29:32 -0700

    These are tough

---
+++ clock.js
@@ -0,0 +1,44 @@
+// var degrees = 45
+var hourHand = document.getElementById("hour");
+var minuteHand = document.getElementById("minute");
+var secondHand = document.getElementById("second");
+
+let sec = 3;
+let min = 10;
+let hour =
+
+
+
+// let secPosition = secondHand.style.transform = "rotate((0/60) *)";
+// let minPosition = minuteHand.style.transform = "rotate(15deg)";
+// let hourPosition = hourHand.style.transform = "rotate(45deg)";
+
+// secPosition
+// minPosition
+// hourPosition
+
+
+// secondHand.style.transform="rotate(" + secPosition + "deg)";
+// minuteHand.style.transform="rotate(" + minPosition + "deg)";
+// hourHand.style.transform="rotate(" + hourPosition + "deg)";
+
+
+
+
+// function run_the_clock(){
+//   var date = new Date();
+//   let hr = date.getHours();
+//   let min = date.getMinutes();
+//   let sec = date.getSeconds();
+//   console.log("Hour: "+hr+ " Minute: "+ min + " Second: "+ sec);
+
+// //   let hrPosition = hr*360/12 + ((min * 360/60)/12) ;
+// //   let minPosition = (min * 360/60) + (sec* 360/60)/60;
+// //   let secPosition = sec * 360/60;
+
+//   //Then we need to apply these numbers as degrees in the inline styles for transform on each of the objects.
+  
+// }
+
+
+// var interval = setInterval(run_the_clock, 1000);
